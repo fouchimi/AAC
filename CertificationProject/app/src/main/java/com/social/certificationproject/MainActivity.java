ca3cfaaa3b4b63e224a24dc42ee0d5cbea0af8c7
fousmane <ousmanefouchimi1990@gmail.com>
2017-02-04 14:58:27 -0600

    Added onclick events on both buttons

--- CertificationProject/app/src/main/java/com/social/certificationproject/MainActivity.java
+++ CertificationProject/app/src/main/java/com/social/certificationproject/MainActivity.java
@@ -1,17 +1,36 @@
 package com.social.certificationproject;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private int mCount = 0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "Hello World");
+        mShowCount = (TextView) findViewById(R.id.show_count);
+        Log.d(TAG, "Hello Toast Project");
+    }
+
+    public void showToast(View view) {
+        Context context = getApplicationContext();
+        Toast.makeText(context, R.string.toast_message, Toast.LENGTH_LONG).show();
+    }
+
+    public void countUp(View view) {
+        if(mShowCount != null){
+            mCount++;
+            mShowCount.setText(Integer.toString(mCount));
+        }
     }
 }
