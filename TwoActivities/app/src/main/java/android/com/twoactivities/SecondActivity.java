7820f96c6a2b3356bf86295898a15eff0613e9f3
fousmane <ousmanefouchimi1990@gmail.com>
2017-02-05 22:46:35 -0600

    Finish TwoActivities project:

---
+++ TwoActivities/app/src/main/java/android/com/twoactivities/SecondActivity.java
@@ -0,0 +1,76 @@
+package android.com.twoactivities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private static final String TAG = SecondActivity.class.getSimpleName();
+    private TextView mTextMessage;
+    public static final String EXTRA_REPLY = "android.com.twoactivities.extra.REPLY";
+    private EditText mReply;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        mTextMessage = (TextView) findViewById(R.id.text_message);
+        mTextMessage.setText(message);
+        mReply = (EditText) findViewById(R.id.editText_second);
+        Log.d(TAG, "onCreate");
+    }
+
+    public void returnReply(View view) {
+        Log.d(TAG, "Replying ...");
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        Log.d(TAG, "End SecondActivity");
+        finish();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+}
